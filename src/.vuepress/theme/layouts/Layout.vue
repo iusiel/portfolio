f88926a092255991fda105db759a073f174f7ae1
Iusiel <iusiel@outlook.com>
2022-06-07 21:10:45 +0800

    Heaader component. Also created additional utility css classes

--- src/.vuepress/theme/layouts/Layout.vue
+++ src/.vuepress/theme/layouts/Layout.vue
@@ -1,5 +1,17 @@
+<script>
+import Header from "./../../components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
 <template>
   <div>
-    <Content />
+    <Header :activeLink="$frontmatter.activeLink" />
+    <div class="container">
+      <Content />
+    </div>
   </div>
 </template>
