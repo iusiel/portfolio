ec598adcc194870b9c15eb4bf10643dd505a166c
Iusiel <iusiel@outlook.com>
2022-06-22 21:10:58 +0800

    Updated footer

--- src/.vuepress/components/Footer.vue
+++ src/.vuepress/components/Footer.vue
@@ -1,11 +1,31 @@
 <script>
 export default {
     name: 'Footer',
+    data: function () {
+        return {
+            copyright: '',
+        };
+    },
+
+    mounted() {
+        const date = new Date();
+        this.copyright = `Copyright © ${date.getFullYear()} Iusiel`;
+    },
 };
 </script>
 
 <template>
     <footer>
-        <div class="footer__content">Created with VuePress.</div>
+        <div class="footer__content">
+            <div>Created with VuePress.</div>
+            <a
+                class="footer__link"
+                href="https://github.com/iusiel"
+                target="_blank"
+                rel="noopener"
+                >Github</a
+            >
+            <div>{{ copyright }}</div>
+        </div>
     </footer>
 </template>
